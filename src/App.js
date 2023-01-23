ea5b1dc977bcd33c4b7b9a4c9868df4b8d5edd78
BRENMA <78454670+BRENMA@users.noreply.github.com>
2023-01-22 22:39:23 -0800

    new thirdweb template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { ConnectWallet } from "@thirdweb-dev/react";
+import "./styles/Home.css";
 
-function App() {
+export default function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="container">
+      <main className="main">
+        <h1 className="title">
+          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
+        </h1>
+
+        <p className="description">
+          Get started by configuring your desired network in{" "}
+          <code className="code">src/index.js</code>, then modify the{" "}
+          <code className="code">src/App.js</code> file!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <div className="connect">
+          <ConnectWallet />
+        </div>
+
+        <div className="grid">
+          <a href="https://portal.thirdweb.com/" className="card">
+            <h2>Portal &rarr;</h2>
+            <p>
+              Guides, references and resources that will help you build with
+              thirdweb.
+            </p>
+          </a>
+
+          <a href="https://thirdweb.com/dashboard" className="card">
+            <h2>Dashboard &rarr;</h2>
+            <p>
+              Deploy, configure and manage your smart contracts from the
+              dashboard.
+            </p>
+          </a>
+
+          <a href="https://portal.thirdweb.com/templates" className="card">
+            <h2>Templates &rarr;</h2>
+            <p>
+              Discover and clone template projects showcasing thirdweb features.
+            </p>
+          </a>
+        </div>
+      </main>
     </div>
   );
 }
-
-export default App;
